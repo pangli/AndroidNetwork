71f7dedd91c882244b5257471b47909f915d1b51
Zorro <zorro.pang@qq.com>
2021-01-07 13:45:50 +0800

    网络请求重试拦截器优化，处理重试成功后请求发送多次问题

--- networking/src/main/java/com/zorro/networking/interceptors/RetryInterceptor.java
+++ networking/src/main/java/com/zorro/networking/interceptors/RetryInterceptor.java
@@ -1,7 +1,6 @@
 package com.zorro.networking.interceptors;
 
 import java.io.IOException;
-import java.io.InterruptedIOException;
 
 import okhttp3.Interceptor;
 import okhttp3.Request;
@@ -14,30 +13,44 @@ import okhttp3.Response;
  * 备注：重试拦截器，重试机制
  */
 public class RetryInterceptor implements Interceptor {
+    private int retryNum = 0;
     private int maxRetry;//最大重试次数,假如设置为3次重试的话，则最大可能请求4次（默认1次+3次重试）
     private long retryInterval;//重试的间隔
 
     public RetryInterceptor(int maxRetry, long retryInterval) {
+        this.retryNum = 0;
         this.maxRetry = maxRetry;
         this.retryInterval = retryInterval;
     }
 
     @Override
     public Response intercept(Chain chain) throws IOException {
+        this.retryNum = 0;
+        return retry(chain);
+    }
+
+    private Response retry(Chain chain) {
         Request request = chain.request();
-        Response response = chain.proceed(request);
-        int retryNum = 0;
-        while (!response.isSuccessful() && retryNum <= maxRetry) {
-            System.out.println("retryNum=" + retryNum);
+        Response response = doRequest(chain, request);
+        while ((response == null || !response.isSuccessful()) && retryNum < maxRetry) {
             try {
                 Thread.sleep(retryInterval);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                throw new InterruptedIOException();
             }
             retryNum++;
-            System.out.println("retryNum=" + retryNum);
+            response = retry(chain);
+        }
+        return response;
+    }
+
+
+    private Response doRequest(Chain chain, Request request) {
+        Response response = null;
+        try {
             response = chain.proceed(request);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return response;
     }
